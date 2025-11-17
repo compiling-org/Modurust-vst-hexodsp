2b708880ec5839f5742338dc787f0b382e7618c5
Sleepless Monk <kapil.bambardekar@gmail.com>
2025-11-17 21:36:52 +0530

    chore: remove accidental 'ref project' directory

--- ref project/modular fractal shader/examples/load_isf_shaders.rs
+++
@@ -1,8 +0,0 @@
-use rust_fractal_shader_engine::test_load_isf_shaders;
-
-fn main() {
-    if let Err(e) = test_load_isf_shaders() {
-        eprintln!("Error loading ISF shaders: {}", e);
-        std::process::exit(1);
-    }
-}
